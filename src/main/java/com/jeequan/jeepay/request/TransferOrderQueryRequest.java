c9a5b85a21d5b6386933e8183dfdac672b9c9132
terrfly <tengfei9753@sina.com>
2021-08-16 17:09:12 +0800

    转账查单接口；

---
+++ src/main/java/com/jeequan/jeepay/request/TransferOrderQueryRequest.java
@@ -0,0 +1,62 @@
+package com.jeequan.jeepay.request;
+
+import com.jeequan.jeepay.Jeepay;
+import com.jeequan.jeepay.model.JeepayObject;
+import com.jeequan.jeepay.net.RequestOptions;
+import com.jeequan.jeepay.response.TransferOrderQueryResponse;
+
+/***
+* Jeepay转账查询请求实现
+*
+* @author terrfly
+* @site https://www.jeepay.vip
+* @date 2021/8/16 16:26
+*/
+public class TransferOrderQueryRequest implements JeepayRequest<TransferOrderQueryResponse> {
+
+    private String apiVersion = Jeepay.VERSION;
+    private String apiUri = "api/transfer/query";
+    private RequestOptions options;
+    private JeepayObject bizModel = null;
+
+    @Override
+    public String getApiUri() {
+        return this.apiUri;
+    }
+
+    @Override
+    public String getApiVersion() {
+        return this.apiVersion;
+    }
+
+    @Override
+    public void setApiVersion(String apiVersion) {
+        this.apiVersion = apiVersion;
+    }
+
+    @Override
+    public RequestOptions getRequestOptions() {
+        return this.options;
+    }
+
+    @Override
+    public void setRequestOptions(RequestOptions options) {
+        this.options = options;
+    }
+
+    @Override
+    public JeepayObject getBizModel() {
+        return this.bizModel;
+    }
+
+    @Override
+    public void setBizModel(JeepayObject bizModel) {
+        this.bizModel = bizModel;
+    }
+
+    @Override
+    public Class<TransferOrderQueryResponse> getResponseClass() {
+        return TransferOrderQueryResponse.class;
+    }
+
+}
---
+++ src/test/java/com/jeequan/jeepay/TransferOrderTest.java
@@ -0,0 +1,94 @@
+package com.jeequan.jeepay;
+
+import com.jeequan.jeepay.exception.JeepayException;
+import com.jeequan.jeepay.model.TransferOrderCreateReqModel;
+import com.jeequan.jeepay.model.TransferOrderQueryReqModel;
+import com.jeequan.jeepay.request.TransferOrderCreateRequest;
+import com.jeequan.jeepay.request.TransferOrderQueryRequest;
+import com.jeequan.jeepay.response.TransferOrderCreateResponse;
+import com.jeequan.jeepay.response.TransferOrderQueryResponse;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+class TransferOrderTest {
+
+    final static Logger _log = LoggerFactory.getLogger(TransferOrderTest.class);
+
+    @BeforeAll
+    public static void initApiKey() {
+        Jeepay.setApiBase(JeepayTestData.getApiBase());
+        Jeepay.apiKey = JeepayTestData.getApiKey();
+        Jeepay.mchNo = JeepayTestData.getMchNo();
+        Jeepay.appId = JeepayTestData.getAppId();
+    }
+
+    @Test
+    public void testTransferOrderCreate() {
+        JeepayClient jeepayClient = new JeepayClient();
+        //jeepayClient.setApiKey("F22nwkjrwre23t552324244");    // 设置apiKey,
+        //jeepayClient.setApiBase("https://pay.jeepay.vip");
+
+        TransferOrderCreateRequest request = new TransferOrderCreateRequest();
+        TransferOrderCreateReqModel model = new TransferOrderCreateReqModel();
+        model.setMchNo(Jeepay.mchNo);                       // 商户号
+        model.setAppId(Jeepay.appId);                       // 应用ID
+        model.setMchOrderNo("mho" + new Date().getTime());                // 商户转账单号
+        model.setAmount(1L);
+        model.setCurrency("CNY");
+        model.setIfCode("wxpay");
+        model.setEntryType("WX_CASH");
+        model.setAccountNo("a6BcIwtTvIqv1zXZohc61biryWok");
+        model.setAccountName("");
+        model.setTransferDesc("测试转账");
+        model.setClientIp("192.166.1.132");                 // 发起转账请求客户端的IP地址
+        request.setBizModel(model);
+
+        try {
+            TransferOrderCreateResponse response = jeepayClient.execute(request);
+            _log.info("验签结果：{}", response.checkSign(Jeepay.apiKey));
+            // 判断转账发起是否成功（并不代表转账成功）
+            if(response.isSuccess(Jeepay.apiKey)) {
+                String transferId = response.get().getTransferId();
+                _log.info("transferId：{}", transferId);
+                _log.info("mchOrderNo：{}", response.get().getMchOrderNo());
+            }else {
+                _log.info("下单失败：mchOrderNo={}, msg={}", model.getMchOrderNo(), response.getMsg());
+                _log.info("通道错误码：{}", response.get().getErrCode());
+                _log.info("通道错误信息：{}", response.get().getErrMsg());
+            }
+        } catch (JeepayException e) {
+            _log.error(e.getMessage());
+        }
+
+    }
+
+    @Test
+    public void testTransferOrderQuery() {
+        JeepayClient jeepayClient = new JeepayClient();
+        TransferOrderQueryRequest request = new TransferOrderQueryRequest();
+        TransferOrderQueryReqModel model = new TransferOrderQueryReqModel();
+        model.setMchNo(Jeepay.mchNo);                                             // 商户号
+        model.setAppId(Jeepay.appId);                                             // 应用ID
+        model.setTransferId("T202108121543441860003");                         // 转账单号
+        request.setBizModel(model);
+        try {
+            TransferOrderQueryResponse response = jeepayClient.execute(request);
+            _log.info("验签结果：{}", response.checkSign(Jeepay.apiKey));
+            if(response.isSuccess(Jeepay.apiKey)) {
+                _log.info("订单信息：{}", response);
+                _log.info("转账状态：{}", response.get().getState());
+                _log.info("转账金额：{}", response.get().getAmount());
+            }
+        } catch (JeepayException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+}
