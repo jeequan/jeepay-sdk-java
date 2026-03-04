3145d920d92bb6c5d5f5b28d262540b9ac204b15
yurong <yurong@jeequan.com>
2026-03-04 14:40:15 +0800

    1.4.5账户余额查询增加不可提现金额

--- src/main/java/com/jeequan/jeepay/model/DivisionReceiverChannelBalanceQueryResModel.java
+++ src/main/java/com/jeequan/jeepay/model/DivisionReceiverChannelBalanceQueryResModel.java
@@ -15,9 +15,14 @@ public class DivisionReceiverChannelBalanceQueryResModel extends JeepayObject {
     private Long receiverId;
 
     /**
-     * 余额, 单位：分
+     * 可提现余额, 单位：分
      */
     private Long balanceAmount;
+
+    /**
+     * 不可提现余额, 单位：分
+     **/
+    private Long unBalanceAmount;
 
     public Long getReceiverId() {
         return receiverId;
@@ -33,5 +38,13 @@ public class DivisionReceiverChannelBalanceQueryResModel extends JeepayObject {
 
     public void setBalanceAmount(Long balanceAmount) {
         this.balanceAmount = balanceAmount;
+    }
+
+    public Long getUnBalanceAmount() {
+        return unBalanceAmount;
+    }
+
+    public void setUnBalanceAmount(Long unBalanceAmount) {
+        this.unBalanceAmount = unBalanceAmount;
     }
 }
